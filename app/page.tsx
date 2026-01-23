e858155b8da7d913aede7d07c404242f779e190c
Joaokiller360 <joao1.joao3@gmail.com>
2026-01-23 12:49:45 -0500

    Update: Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Inicio, Logos, About, WhatDeDo, WhyChooseUs, Galery } from '@/app/compo
 import { createMetadata } from '@/app/utils'
 
 export const metadata = createMetadata({
-  title: '',
+  title: 'JB.SKYLENS | Servicios profesionales con drones en Ecuador',
   description:
     'JB.SKYLENS ofrece servicios profesionales con drones en Ecuador: inspecciones, fotografía aérea, video, eventos y soluciones técnicas con drones.',
   keywords: [
