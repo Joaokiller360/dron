40bbca65f4c1b12e3d40a8c406c9cd548ff670c6
Joaokiller360 <joao1.joao3@gmail.com>
2026-02-08 15:13:07 -0500

    Update: Page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Script from "next/script";
+
 import { createMetadata, Navbar } from '@/app/utils'
 import { Footer } from '@/app/component';
 import FlowbiteInit from "./flowbait-init";
@@ -28,6 +30,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.GOOGLE_ANALYTICS}');
+          `}
+      </Script>
       <meta name="google-site-verification" content={process.env.GOOGLE_VERIFICATION} />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
